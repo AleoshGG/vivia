264ed7a407754fe1a2285a04f6d47a3765937d9b
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-03-08 23:12:08 -0600

    Start project

---
+++ src/main/java/aleosh/online/vivia/ViviaApplication.java
@@ -0,0 +1,13 @@
+package aleosh.online.vivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViviaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aleosh/online/vivia/ViviaApplicationTests.java
@@ -0,0 +1,13 @@
+package aleosh.online.vivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViviaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
