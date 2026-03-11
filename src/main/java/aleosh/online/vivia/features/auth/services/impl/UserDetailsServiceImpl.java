08effa4f1c0fabcc1e01fe30f49553b60ecba9d1
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-03-11 12:19:02 -0600

    Corrigiendo transacciones

--- src/main/java/aleosh/online/vivia/features/auth/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/aleosh/online/vivia/features/auth/services/impl/UserDetailsServiceImpl.java
@@ -9,10 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final ILessorRepository lessorRepository;
