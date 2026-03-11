a795f9dd57c353c09cb82ff485ba76c459a43ffc
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-03-11 03:01:49 -0600

    Obteniendo credenciales

--- src/main/java/aleosh/online/vivia/core/config/firebase/FirebaseConfig.java
+++ src/main/java/aleosh/online/vivia/core/config/firebase/FirebaseConfig.java
@@ -4,19 +4,25 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials.path:classpath:firebase-adminsdk.json}")
+    private Resource firebaseCredentials;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
+            InputStream serviceAccount = firebaseCredentials.getInputStream();
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("firebase-adminsdk.json").getInputStream()))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
             return FirebaseApp.initializeApp(options);
         }
