b923b47fcaa45dee6347933f3da63c7dfc8b3e62
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-03-11 01:50:27 -0600

    Liberando /properties

--- src/main/java/aleosh/online/vivia/core/config/security/SecurityConfig.java
+++ src/main/java/aleosh/online/vivia/core/config/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/lessors/register/**").permitAll()
-                        .requestMatchers("/properties").permitAll()
+                        .requestMatchers("/properties/**").permitAll()
                         .requestMatchers("/lessees/register/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/.well-known/assetlinks.json").permitAll()
