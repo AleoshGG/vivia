b702250ea8ec91945c91cc3948e21f077dd0c965
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-04-13 23:48:45 -0600

    Configurando credenciales

--- src/main/java/aleosh/online/vivia/core/config/aws/AwsConfig.java
+++ src/main/java/aleosh/online/vivia/core/config/aws/AwsConfig.java
@@ -1,19 +1,45 @@
 package aleosh.online.vivia.core.config.aws;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsConfig {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.credentials.session-token:}")
+    private String sessionToken;
+
+    @Value("${cloud.aws.region.static:us-east-1}")
+    private String region;
+
     @Bean
     public S3Client s3Client() {
-        // El cliente por defecto busca automáticamente las variables de entorno:
-        // AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY y AWS_SESSION_TOKEN
+        AwsCredentialsProvider credentialsProvider;
+
+        if (sessionToken != null && !sessionToken.isEmpty()) {
+            credentialsProvider = StaticCredentialsProvider.create(
+                    AwsSessionCredentials.create(accessKey, secretKey, sessionToken));
+        } else {
+            credentialsProvider = StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(accessKey, secretKey));
+        }
+
         return S3Client.builder()
-                .region(Region.US_EAST_1) // Asegúrate que coincida con tu bucket
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
